524dee0c58029079eec11857a8330efd3cb6b7df
peng1232 <1439438940@qq.com>
2024-04-03 09:08:44 +0800

    更新数据库表

--- src/main/java/com/entity/Product.java
+++ src/main/java/com/entity/Product.java
@@ -14,6 +14,7 @@ public class Product {
 	private Integer category_id;//类别表id
 	private String description;//商品描述
 	private Long hits;//点击量
+	private Integer purchase_limitInteger;//购买限制
 	private Timestamp listing_time;//上架时间 当状态变成1的时候，就更改
 	private Integer state;//0：待上架 1：上架 2：下架/缺货
 	public Integer getId() {
@@ -58,26 +59,34 @@ public class Product {
 	public void setState(Integer state) {
 		this.state = state;
 	}
+	
+	public Integer getPurchase_limitInteger() {
+		return purchase_limitInteger;
+	}
+	public void setPurchase_limitInteger(Integer purchase_limitInteger) {
+		this.purchase_limitInteger = purchase_limitInteger;
+	}
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	@Override
 	public String toString() {
 		return "Product [id=" + id + ", products_name=" + products_name + ", category_id=" + category_id
-				+ ", description=" + description + ", hits=" + hits + ", listing_time=" + listing_time + ", state="
-				+ state + "]";
+				+ ", description=" + description + ", hits=" + hits + ", purchase_limitInteger=" + purchase_limitInteger
+				+ ", listing_time=" + listing_time + ", state=" + state + "]";
 	}
 	public Product(Integer id, String products_name, Integer category_id, String description, Long hits,
-			Timestamp listing_time, Integer state) {
+			Integer purchase_limitInteger, Timestamp listing_time, Integer state) {
 		super();
 		this.id = id;
 		this.products_name = products_name;
 		this.category_id = category_id;
 		this.description = description;
 		this.hits = hits;
+		this.purchase_limitInteger = purchase_limitInteger;
 		this.listing_time = listing_time;
 		this.state = state;
-	}
-	public Product() {
-		super();
-		// TODO Auto-generated constructor stub
 	}
 	
 }
