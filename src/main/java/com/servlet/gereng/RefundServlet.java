59931dab0e43da1d048cc5a1e47e51f3ee059fac
Peng <2925556821@qq.com>
2024-04-08 08:35:45 +0800

    提

--- src/main/java/com/servlet/gereng/RefundServlet.java
+++ src/main/java/com/servlet/gereng/RefundServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +14,7 @@ import com.alibaba.fastjson2.JSONObject;
 import com.dao.RefundDAO;
 import com.entity.Refund;
 
-
+@WebServlet("/jsp/refund")
 public class RefundServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	RefundDAO r = new RefundDAO();
