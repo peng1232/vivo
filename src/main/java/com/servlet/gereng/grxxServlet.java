b6d241f550b41b3e5ac8412fa403a3250c85a628
Peng <2925556821@qq.com>
2024-04-11 15:02:35 +0800

    1

--- src/main/java/com/servlet/gereng/grxxServlet.java
+++ src/main/java/com/servlet/gereng/grxxServlet.java
@@ -20,18 +20,15 @@ public class grxxServlet extends HttpServlet{
 	UsersDAO r = new UsersDAO();
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		List<Users> city = r.selectAll();
-		JSONArray jsonArray = new JSONArray();
-		city.forEach(e->{
-			JSONObject jsonObj = new JSONObject();
-			jsonObj.put("id",e.getId());
-			jsonObj.put("user_nickname",e.getUser_nickname());
-            jsonObj.put("phone", e.getPhone());
-            jsonObj.put("head_sculpture",e.getHead_sculpture());
-            jsonObj.put("birth",e.getBirth());
-            jsonObj.put("sex",e.getSex());
-            jsonArray.add(jsonObj);
-		});
+		/*
+		 * List<Users> city = r.selectAll(); JSONArray jsonArray = new JSONArray();
+		 * city.forEach(e->{ JSONObject jsonObj = new JSONObject();
+		 * jsonObj.put("id",e.getId());
+		 * jsonObj.put("user_nickname",e.getUser_nickname()); jsonObj.put("phone",
+		 * e.getPhone()); jsonObj.put("head_sculpture",e.getHead_sculpture());
+		 * jsonObj.put("birth",e.getBirth()); jsonObj.put("sex",e.getSex());
+		 * jsonArray.add(jsonObj); });
+		 */
 		resp.setContentType("application/json");
 		resp.setCharacterEncoding("UTF-8");
 		resp.getWriter().println(jsonArray.toJSONString());
