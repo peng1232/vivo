f30c2c223fb37160a7767a0ac561ab9e368895db
peng1232 <1439438940@qq.com>
2024-04-12 08:26:05 +0800

    1

--- src/main/java/com/servlet/product/IsShouServlet.java
+++ src/main/java/com/servlet/product/IsShouServlet.java
@@ -17,15 +17,19 @@ import com.dao.CollectionDAO;
 public class IsShouServlet extends HttpServlet {
 	CollectionDAO cdao = new CollectionDAO();
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Integer user_id = Integer.valueOf(request.getParameter("user_id").trim());
-		Integer product_id = Integer.valueOf(request.getParameter("product_id"));
-		String sku = request.getParameter("sku");
-		Boolean collection = cdao.isCollection(user_id, product_id, sku);
-		JSONObject jsonObj = new JSONObject();
-		jsonObj.put("collection", collection);
-		response.setContentType("application/json");
-		response.setCharacterEncoding("UTF-8");
-		response.getWriter().println(jsonObj.toJSONString());
+		String zhi = request.getParameter("user_id");
+		if(zhi!=null&&zhi.length()>0&&zhi!=" ") {
+			Integer user_id = Integer.valueOf(zhi.trim());
+			Integer product_id = Integer.valueOf(request.getParameter("product_id"));
+			String sku = request.getParameter("sku");
+			Boolean collection = cdao.isCollection(user_id, product_id, sku);
+			JSONObject jsonObj = new JSONObject();
+			jsonObj.put("collection", collection);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().println(jsonObj.toJSONString());
+		}
+		
 	}
 
 }
